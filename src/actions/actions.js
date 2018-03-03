1258a2bca60d7c46a484795161517c6d964c5644
Leonardo Espinoza <espinoza.lespinoza@gmail.com>
2018-03-03 14:28:51 -0300

    Se agregan todos los casos de conectividad a servidor de películas

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,4 +3,6 @@ import { createAction } from 'redux-actions';
 export const getVideos = createAction(
 	'GET_VIDEOS', 
 	() => fetch('https://demo2697834.mockable.io/movies').then(response => response.json())
+//	() => fetch('https://www.google.cl').then(response => response.json())
+//	() => fetch('https://noexisto.cl').then(response => response.json()) //.catch(e => e) // No poner catch, porque sino, no resultará en GET_VIDEOS_REJECTED
 )
--- src/components/CarrouselVideos.jsx
+++ src/components/CarrouselVideos.jsx
@@ -2,9 +2,97 @@ import React from 'react';
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
-
+/**
+{
+	"totalCount": 30,
+	"entries": [
+		{
+			"title": "10 Things I Hate About You",
+			"description": "A new kid must find a guy to date the meanest girl in school, the older sister of the girl he has a crush on, who cannot date until her older sister does.",
+			"type": "movie",
+			"publishedDate": 931478400000,
+			"availableDate": 931478400000,
+			"metadata": [
+				{
+					"value": "en",
+					"name": "language"
+				}
+			],
+			"contents": [
+				{
+					"url": "http://d2bqeap5aduv6p.cloudfront.net/project_coderush_640x360_521kbs_56min.mp4",
+					"format": "mp4",
+					"width": 640,
+					"height": 360,
+					"language": "en",
+					"duration": 3600000,
+					"geoLock": false,
+					"id": "vid_103"
+				}
+			],
+			"credits": [
+				{
+					"role": "Director",
+					"name": "Gil Junger"
+				},
+				{
+					"role": "Heath Ledger",
+					"name": "Heath Ledger"
+				},
+				{
+					"role": "Julia Stiles",
+					"name": "Julia Stiles"
+				},
+				{
+					"role": "Joseph Gordon-Levitt",
+					"name": "Joseph Gordon-Levitt"
+				}
+			],
+			"parentalRatings": [
+				{
+					"scheme": "MPAA",
+					"rating": "PG_13"
+				}
+			],
+			"images": [
+				{
+					"type": "cover",
+					"url": "http://lorempixel.com/214/317/?t=1",
+					"width": 214,
+					"height": 317,
+					"id": "f67e6e8a7478d1dae24e869f3d7081cf"
+				}
+			],
+			"categories": [
+				{
+					"title": "Comedy",
+					"description": "Comedy movies",
+					"id": "movies-comedy"
+				},
+				{
+					"title": "Drama",
+					"description": "Drama movies",
+					"id": "movies-drama"
+				},
+				{
+					"title": "Romance",
+					"description": "Romantic movies",
+					"id": "movies-romance"
+				}
+			],
+			"id": "10-things-i-hate-about-you"
+		},
+		...
+	]
+}
+ */
 const mapStateToProps = (state) => {
 	return {
+		procesamiento: {
+			enProceso: state.getVideos.pendingRequest,
+			ultimoProcesoOk: state.getVideos.successfulRequest,
+			ultimoProcesoError: state.getVideos.error
+		},
     	peliculas: state.getVideos.videos
     }
 }
@@ -17,18 +105,35 @@ const mapDistpatchToProps = null;
 	}
 }*/
 
-const CarrouselVideos = ({ peliculas }) => (
-	<ul>
-		{peliculas.map((pelicula, idx) => 
-			<li key={idx}>{pelicula.title}</li>
-		)}
-	</ul>
-);
+const CarrouselVideos = ({ procesamiento: { enProceso, ultimoProcesoOk, ultimoProcesoError }, peliculas }) => {
+	if (enProceso) return (
+		<p>En Proceso</p>
+	) 
+	if (ultimoProcesoOk) {
+		if ((typeof peliculas == "undefined") || (peliculas === null) || !Array.isArray(peliculas) || (peliculas.length == 0)) return (
+			<p>No se encontraron peliculas.</p>
+		)
+		return (
+		<ul>
+			{peliculas.map((pelicula, idx) => 
+				<li key={idx}>{pelicula.title}</li>
+			)}
+		</ul>
+		)
+	}
+	console.log({
+		title: "Error conexion servidor peliculas.", 
+		error: ultimoProcesoError,
+		merr: ultimoProcesoError.message
+	})
+	return (
+		<p>Error al traer peliculas desde servidor.</p>
+	)
+};
 
 CarrouselVideos.propTypes = {
+	procesamiento: PropTypes.object,
 	peliculas: PropTypes.arrayOf(PropTypes.object)
 };
 
-const CarrouselVideosComponent = connect(mapStateToProps, mapDistpatchToProps)(CarrouselVideos)
-
-export default CarrouselVideosComponent
+export default connect(mapStateToProps, mapDistpatchToProps)(CarrouselVideos)
