01c5b018e7f29b91354bd44e62b63dfa060d4666
Leonardo Espinoza <espinoza.lespinoza@gmail.com>
2018-03-31 22:24:09 -0300

    Ajustes de presentación, mostrando imagen en cada video de carrousel, y ajustando estilos css de letra para el título

--- src/components/PeliculaPresentacion.jsx
+++ src/components/PeliculaPresentacion.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
+
+const putImageUrl = (data) => {
+	if ((typeof data.images === 'undefined') ||
+		(!Array.isArray(data.images)) ||
+		(data.images.length == 0) ||
+		(typeof data.images[0].url === 'undefined') ||
+		(data.images[0].url == '')
+	   ) return undefined;
+	return 'url(' + data.images[0].url + ')'
+}
+
+
+
 const mapStateToProps = (state, ownProps) => {
 	return ownProps
 }
@@ -15,7 +28,13 @@ const mapDistpatchToProps = null;
 }*/
 
 const PeliculaPresentacion = (props) => (
-	<div className="video" key={props.key}>{props.data.title}</div>
+	<div className="video" 
+		 key={props.key} 
+		 style={{ 
+		 	backgroundImage: putImageUrl(props.data) 
+		 }}>
+		<div className="videoTitle">{props.data.title}</div>
+	</div>
 )
 
 PeliculaPresentacion.propTypes = {
